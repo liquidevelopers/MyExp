e6f5fba4282fcfabd8b92db452449aee1508b583
Advaith <Advaith@Advaith-PC>
2019-01-08 12:45:05 +0530

    ks

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -23,6 +23,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
   return self.registration.showNotification(payload.data.title,
 		  {
 	    body: payload.data.body
+	    
 	    /*icon: payload.data.icon,
 	    tag: payload.data.tag,
 	    data: payload.data.link*/
